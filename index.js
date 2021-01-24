155839c63fe82e5adff2a4cf141db811cb0d4f65
Jammie <73665246+jammiehardy2@users.noreply.github.com>
2021-01-23 19:40:13 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const inquirer = require('inquirer');
+const fs = require("fs");
+const util = require("util");
+const inquirer = require("inquirer");
+const generateReadme = require("./utils/generateReadme")
+const writeFileAsync = util.promisify(fs.writeFile);
+
