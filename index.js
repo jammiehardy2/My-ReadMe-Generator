6042db41635bcd7ec6d44fe23c9ddd5a4aa9cdfe
Jammie <73665246+jammiehardy2@users.noreply.github.com>
2021-01-23 20:39:22 -0700

    Add files via upload
    Adding licences and their https sources

--- index.js
+++ index.js
@@ -59,6 +59,7 @@ const promptUser = () => {
  ]
 
 }),
+//Adding licences and their function?
 //[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
 //[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)
 //[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
